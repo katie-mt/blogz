70dc1d3deaf8d005a8f735e4990de075670ba0c6
Katie O'Brien <katie.obrien.mt@gmail.com>
2017-07-22 14:35:13 -0700

    updated email fields to read as username

--- main.py
+++ main.py
@@ -36,18 +36,18 @@ class User(db.Model):
 
     #specify the datafields that should go into columns.  Set primary key
     id = db.Column(db.Integer, primary_key=True)
-    #our users will have email and password properties
-    #we shouldn't have two different accounts associated with an email.  unique=True ensures that SQLAlchemy will not allow the creation of two separate users with the same email. 
-    email = db.Column(db.String(120), unique=True)
+    #our users will have username and password properties
+    #we shouldn't have two different accounts associated with a username.  unique=True ensures that SQLAlchemy will not allow the creation of two separate users with the same username. 
+    username = db.Column(db.String(120), unique=True)
     password = db.Column(db.String(120))
     #specify that a user can have many blogs.  This isn't going to be a column because there won't be a column in the database for this.  But we want to establish that there is a relationship here that we want the relational database manager SQLAlchemy to manage for us.  
     #SQLAlchemy should populate this blogs list with things from the class task such that their owner property is equal to the specific user that is in consideration. 
     blogs = db.relationship('Blog', backref='owner')
 
     #initializer (constructor) for the user class
-    #users should always have emails and passwords so we'll add those as parameters to our constructor
-    def __init__(self, email, password):
-        self.email = email
+    #users should always have usernames and passwords so we'll add those as parameters to our constructor
+    def __init__(self, username, password):
+        self.username = username
         self.password = password
         #we then went into python shell to create the user table and at least one user object for us to work with.  See video 5 for details.
 
@@ -58,21 +58,21 @@ class User(db.Model):
 def login():
     #check for the request type.  On a GET request I just want to render the login form and on a POST request I want to get data out of the request and try to log the user in.
     if request.method == 'POST':
-        #remember that there are two inputs on the login form, one that is name=email and one that is name=password.  Get this data out of the request
+        #remember that there are two inputs on the login form, one that is name=username and one that is name=password.  Get this data out of the request
         ##request.form is the python dictionary that contains Post data or data sent in a post request
-        email = request.form['email']
+        username = request.form['username']
         password = request.form['password']
-        #try to verify the user's password.  Need to retrieve the user and the associated email from the database. 
+        #try to verify the user's password.  Need to retrieve the user and the associated username from the database. 
         #.first.  If we expect that our result set will only contain one thing or we want to get the first thing. This will retrieve that one item
-        #this will return a user with the given email with the given email if the user exists.  If the user doesn't exist then the user variable will be equal to the special value none. 
-        user = User.query.filter_by(email=email).first()
-        #to verify that not only do i have the correct password but do I have a user in the system with the given email
-        #this will check first, does the user exist.  If this is none (query did not return a user with the given email address from the database) then this will be none and the conditional will short circuit.
+        #this will return a user with the given username with the given username if the user exists.  If the user doesn't exist then the user variable will be equal to the special value none. 
+        user = User.query.filter_by(username=username).first()
+        #to verify that not only do i have the correct password but do I have a user in the system with the given username
+        #this will check first, does the user exist.  If this is none (query did not return a user with the given username from the database) then this will be none and the conditional will short circuit.
         #if the user does exist then we will go ahead and compare the password.  At which point we would want to login
         if user and user.password == password:
             #a session is an object that you can use to store data that is associated with a specific user from one request to another. 
-            #when a user logs in, put in session (session is a dictionary) a piece of data.  Under email input the email.  Add this both here and under register.
-            session['email'] = email
+            #when a user logs in, put in session (session is a dictionary) a piece of data.  Under username input the username.  Add this both here and under register.
+            session['username'] = username
             #here we want to give the user a message that says they are logged in.  This puts the message in a queue for us to access within our base template
             flash("Logged in")
             #when a user logs in, we are going to redirect them to the homepage
@@ -90,22 +90,22 @@ def signup():
 
     if request.method == 'POST':
     #if we are dealing with a request method of post then create a new user
-    #in the registration template we have an email, password and verify.  Retrieve those parameters from the database
-        email = request.form['email']
+    #in the registration template we have a username, password and verify.  Retrieve those parameters from the database
+        username = request.form['username']
         password = request.form['password']
         verify = request.form['verify']
 
         #if we validate the data and everything is good, then go ahead and create a new user
         #check that the user does not exist
-        existing_user = User.query.filter_by(email=email).first()
+        existing_user = User.query.filter_by(username=username).first()
         #if there is not an existing user then create a new user from the input information
         if not existing_user:
-            new_user = User(email, password)
+            new_user = User(username, password)
             db.session.add(new_user)
             db.session.commit()
-            #we put this above under login as well.  Once a user registers and we put them in the database, we'll log them in by putting their email in the session.
+            #we put this above under login as well.  Once a user registers and we put them in the database, we'll log them in by putting their username in the session.
             #cookies will track who is logging in so server knows who to automatically log in.
-            session['email'] = email
+            session['username'] = username
             #if there is not an existing user, redirect the user back to the root
             return redirect('/blog')
         else:
@@ -137,8 +137,8 @@ def require_login():
     #create a whitelist or list of pages that you don't need to be logged in to view
     #list called allowed routes.  List of routes that users don't have to be logged in to see.  Don't need to login to see the login route or the register route
     allowed_routes = ['login', 'signup']
-    #if I say that the endpoint is not login and not register (request.endpoint is not in the allowed routes) then this means that i'm wanting to force the user to login meaning i should also check if the email is in session.  If the request.endpoint is in the allowed routes then skip over the redirect and process requests as previously
-    if request.endpoint not in allowed_routes and 'email' not in session:
+    #if I say that the endpoint is not login and not register (request.endpoint is not in the allowed routes) then this means that i'm wanting to force the user to login meaning i should also check if the username is in session.  If the request.endpoint is in the allowed routes then skip over the redirect and process requests as previously
+    if request.endpoint not in allowed_routes and 'username' not in session:
     #if the user has not logged in yet
         #redirect to the login page
         return redirect('/login')
@@ -170,8 +170,8 @@ def new_blog():
         
         else:
             #creates blog entry that is a blog entry object
-            email = session['email']
-            owner = User.query.filter_by(email=email).first()
+            username = session['username']
+            owner = User.query.filter_by(username=username).first()
             
             new_blog_entry = Blog(title, blog_entry, owner)
             #put the new_blog_entry in the database
